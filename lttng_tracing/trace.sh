e35b2ec856295c59a8e83597cb392ec75012e87b
khoitd1997 <khoidinhtrinh@gmail.com>
2021-12-02 21:55:46 -0800

    Added some trace stuff

--- lttng_tracing/trace.sh
+++ lttng_tracing/trace.sh
@@ -7,14 +7,26 @@ script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 session_name="kernel_trace_session"
 
 set +e
-lttng destroy ${session_name}
+sudo lttng destroy ${session_name}
 set -e
 
 sudo rm -rf ${script_dir}/trace_output
 sudo lttng create ${session_name} --output=${script_dir}/trace_output
-sudo lttng enable-event --kernel sched_switch,sched_process_fork
+sudo lttng enable-channel --kernel kernel_channel --num-subbuf=4 --subbuf-size=15M
+sudo lttng enable-event --kernel -c kernel_channel -a
+sudo lttng add-context -k -c kernel_channel -t callstack-kernel
+sudo lttng add-context -k -c kernel_channel -t callstack-user
 
 sudo lttng start ${session_name}
-sleep 2
+
+# liblttng-ust-libc-wrapper
+# liblttng-ust-pthread-wrapper
+# liblttng-ust-cyg-profile
+# liblttng-ust-cyg-profile-fast
+# liblttng-ust-dl
+
+LD_PRELOAD="liblttng-ust-cyg-profile-fast.so" ${script_dir}/test_cpp_app/build/test_cmake_app_exe
 sudo lttng destroy ${session_name}
 sudo chown -R ${USER} ${script_dir}/trace_output
+
+sudo cat /proc/kallsyms > ${script_dir}/kernel_symbols.txt
