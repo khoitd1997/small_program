f73d1b8f636a29754bdb01dc42dc07e836788e0d
khoitd1997 <khoidinhtrinh@gmail.com>
2021-12-26 14:37:19 -0800

    got qemu run working

--- qemu_custom_kernel/common.sh
+++ qemu_custom_kernel/common.sh
@@ -6,3 +6,7 @@ buildroot_dir="${root_dir}/buildroot-2021.08.1"
 configs_path="${root_dir}/configs"
 buildroot_defconfig_path="${configs_path}/buildroot_x86_64_defconfig"
 kernel_defconfig_path="${configs_path}/kernel_x86_64_defconfig"
+
+buildroot_out_images_dir="${buildroot_dir}/output/images"
+buildroot_initrd_out_path="${buildroot_out_images_dir}/rootfs.cpio.gz"
+buildroot_kernel_vmlinux_out_path="${buildroot_out_images_dir}/bzImage"
--- qemu_custom_kernel/run_qemu.sh
+++ qemu_custom_kernel/run_qemu.sh
@@ -3,16 +3,13 @@
 set -e
 
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${script_dir}/common.sh"
 
-buildroot_dir="${script_dir}/buildroot-2021.08.1"
-kernel_path="vmlinuz-5.4.0-21"
-initrd_path="/boot/initrd.img-5.4.0-21-workload"
-
-if [ ! -f "${kernel_path}" ]; then
+if [ ! -f "${buildroot_kernel_vmlinux_out_path}" ]; then
     echo "Invalid kernel path"
     exit 1
 fi
 
 
 # references: https://ubuntu.com/server/docs/virtualization-qemu
-sudo qemu-system-x86_64 -M test_computer -cpu host -m 1024 -enable-kvm -serial mon:stdio -nographic -display curses -append 'console=ttyS0,115200,8n1' -kernel "${kernel_path}" -initrd "${initrd_path}"
+qemu-system-x86_64 -cpu host -m 4096 -enable-kvm -serial mon:stdio -nographic -display curses -append 'console=ttyS0,115200,8n1' -kernel "${buildroot_kernel_vmlinux_out_path}" -initrd "${buildroot_initrd_out_path}"
