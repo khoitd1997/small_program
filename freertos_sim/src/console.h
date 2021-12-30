46e528d1e76a12bd61f9e2c99ac4dc67a1f08dc1
khoitd1997 <khoidinhtrinh@gmail.com>
2021-12-30 13:29:09 -0800

    more simplification

--- freertos_sim/src/console.h
+++ freertos_sim/src/console.h
@@ -24,23 +24,7 @@
  *
  */
 
-#ifndef CONSOLE_H
-    #define CONSOLE_H
+#pragma once
 
-    #ifdef __cplusplus
-        extern "C" {
-    #endif
-
-/*-----------------------------------------------------------
-* Example console I/O wrappers.
-*----------------------------------------------------------*/
-
-    void console_init( void );
-    void console_print( const char * fmt,
-                        ... );
-
-    #ifdef __cplusplus
-        }
-    #endif
-
-#endif /* CONSOLE_H */
+void console_init(void);
+void console_print(const char* fmt, ...);
