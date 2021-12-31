d630dfedc5b873eac48e734f97c995ff393c1067
khoitd1997 <khoidinhtrinh@gmail.com>
2021-12-31 09:32:44 -0800

    Added custom r5 library

--- r5_qemu/build.tcl
+++ r5_qemu/build.tcl
@@ -1,15 +1,18 @@
 setws ./r5_workspace
-platform create -name HW1 -hw ./zcu102.xsa -no-boot-bsp
+platform create -name r5_platform -hw ./zcu102.xsa -no-boot-bsp
 # to get the list of available options, just enter some nonsense name and the error message should
 # show the possible options
 # valid OS are "freertos10_xilinx freertos standalone linux"
 domain create -name r5_freertos -os freertos10_xilinx -proc psu_cortexr5_0
 domain active r5_freertos
 
+hsi::set_repo_path ../my_vitis_repo
+
 # Availabel libraries are : "libmetal lwip211 openamp xilrsa xilpm xilfpga xilnvm xilffs xilisf xilflash xilpuf xilskey xilsecure"
 bsp setlib -name libmetal
-bsp setlib -name lwip211
+bsp setlib -name my_lwip211
 bsp setlib -name openamp
+
 
 platform generate
 # app create -name r5_test_app -platform HW1 -template "Zynq MP FSBL" -domain r5_freertos -lang skdafjlasdjfkla
