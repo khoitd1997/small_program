7b0138086f6702453f8e4f1b7ea3cb4e0f11723d
khoitd1997 <khoidinhtrinh@gmail.com>
2021-12-30 22:43:29 -0800

    added build script for qemu dts

--- r5_qemu/run_r5_qemu.sh
+++ r5_qemu/run_r5_qemu.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-# TODO: Try qemu-r5
+script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+qemu_dts_repo_dir="${script_dir}/qemu-devicetrees-xilinx-v2020.1"
+zcu102_dtb_path="${qemu_dts_repo_dir}/LATEST/MULTI_ARCH/zcu102-arm.dtb"
+
+cd "${qemu_dts_repo_dir}"
+make
+
 # looks like qemu-system-aarch64 is also used for running on the R5
-# /tools/Xilinx/Vitis/2020.1/data/emulation/qemu/unified_qemu/sysroots/x86_64-petalinux-linux/usr/bin/qemu-system-aarch64
 # https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/821854273/Running+Bare+Metal+Applications+on+QEMU
 # https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/821952548/Chapter+3+-+Developing+with+QEMU+Virtual+Machines
 /tools/Xilinx/Vitis/2020.1/data/emulation/qemu/unified_qemu/sysroots/x86_64-petalinux-linux/usr/bin/qemu-system-aarch64 \
@@ -13,6 +18,6 @@ set -e
 -device loader,file=<baremetal_for_zynqmp_r5.elf>,cpu-num=4 \
 -device loader,addr=0XFF5E023C,data=0x80088fde,data-len=4 \
 -device loader,addr=0xff9a0000,data=0x80000218,data-len=4 \
--hw-dtb <device tree binary for Versal> \
+-hw-dtb "${zcu102_dtb_path}" \
 -m 4048 \
 -display none
