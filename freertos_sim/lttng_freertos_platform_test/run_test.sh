822c8410b166b1ef943be959fc74600e69648fc4
khoitd1997 <khoidinhtrinh@gmail.com>
2021-12-31 19:37:54 -0800

    refactored freertos_sim

--- freertos_sim/lttng_freertos_platform_test/run_test.sh
+++ freertos_sim/lttng_freertos_platform_test/run_test.sh
@@ -6,20 +6,20 @@ set -e
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 build_dir="${script_dir}/build"
 trace_dir="${script_dir}/trace"
-src_dir="${script_dir}/src"
+src_dir="${script_dir}"
 
 rm -rf "${trace_dir}"
 mkdir "${trace_dir}"
-kernel_trace_dir="${trace_dir}/kernel_trace"
-user_trace_dir="${trace_dir}/user_trace"
 
 rm -rf "${build_dir}"
 mkdir -p "${build_dir}"
 cd "${build_dir}"
 cmake -G "Ninja" -DTRACE_ROOT_DIR="${trace_dir}" -DCMAKE_BUILD_TYPE=Debug "${src_dir}" && cmake --build .
 
-# gdb ./freertos_posix_demo
-TRACE_ROOT_DIR="${trace_dir}" ./freertos_posix_demo
+# gdb ./fbarectf_lttng_freertos_platform_test
+TRACE_ROOT_DIR="${trace_dir}" ./barectf_lttng_freertos_platform_test
 
 echo "Reading trace using babeltrace"
+kernel_trace_dir="${trace_dir}/kernel_trace"
+user_trace_dir="${trace_dir}/user_trace"
 babeltrace2 "${kernel_trace_dir}"
--- freertos_sim/src/README.md
+++
@@ -1,3 +0,0 @@
-# FreeRTOS Sim
-
-Contain a heavily stripped down version of the FreeRTOS Posix demo for experimentation and quick prototyping with FreeRTOS on Linux
