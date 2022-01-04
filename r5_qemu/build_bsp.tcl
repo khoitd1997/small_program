71f6323a53df0a370e8fcbf16c8bec076d2965cf
khoitd1997 <khoidinhtrinh@gmail.com>
2022-01-03 23:35:12 -0800

    Added test to use custom version of FreeRTOS

--- r5_qemu/build_bsp.tcl
+++ r5_qemu/build_bsp.tcl
@@ -7,16 +7,18 @@ file delete -force ./r5_workspace
 
 setws ./r5_workspace
 platform create -name r5_platform -hw "$xsa_file_path" -no-boot-bsp
+
+# NOTE: have to add custom repo early to make sure domain create use our copy of freeRTOS
+hsi::set_repo_path "$custom_repo_path"
+# some weird things happen sometimes if we don't add a short delay after
+# setting the repo path
+after 1000
+
 # to get the list of available options, just enter some nonsense name and the error message should
 # show the possible options
 # valid OS are "freertos10_xilinx freertos standalone linux"
 domain create -name r5_freertos -os freertos10_xilinx -proc psu_cortexr5_0
 domain active r5_freertos
-
-hsi::set_repo_path "$custom_repo_path"
-# some weird things happen if we don't add a short delay after
-# setting the repo path
-after 1000
 
 # Availabel libraries are : "libmetal lwip211 openamp xilrsa xilpm xilfpga xilnvm xilffs xilisf xilflash xilpuf xilskey xilsecure"
 bsp setlib -name libmetal
