d6e550addd73bf15c5cc6722bd69521d906fb75e
khoitd1997 <khoidinhtrinh@gmail.com>
2022-01-23 15:16:34 -0800

    added more crop test stuff

--- yocto_crop_test/run_docker.sh
+++ yocto_crop_test/run_docker.sh
@@ -6,14 +6,18 @@ curr_script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && 
 workdir="${curr_script_dir}/workdir"
 scripts_dir="${curr_script_dir}/scripts"
 
+# change between podman and Docker
+# container_cmd="sudo docker"
+container_cmd="podman"
+
 rm -rf "${workdir}"
 mkdir -p "${workdir}"
 
 # the default image doesn't allow installing additional tool
 # so we have to extend it a bit and then build our own
-sudo docker build -t yocto_build_container - < "${curr_script_dir}/Dockerfile"
+${container_cmd} build -t yocto_build_container - < "${curr_script_dir}/Dockerfile"
 
-sudo docker run --rm -it \
+${container_cmd} run --rm -it \
     -v "${workdir}":/workdir \
     -v "${scripts_dir}":/scripts \
     --workdir=/workdir \
---
+++ yocto_crop_test/test_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# this script is used to test the stability of using Docker
+# by rerunning the build over and over
+
+set -e
+
+curr_script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+counter=0
+
+while :
+do
+    echo "On test iteration number ${counter}"
+	bash "${curr_script_dir}/run_docker.sh"
+    (( counter++ ))
+done
