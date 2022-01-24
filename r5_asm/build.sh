fca8c9a292cccef0138bbd160d870037ac88807c
khoitd1997 <khoidinhtrinh@gmail.com>
2022-01-23 20:57:23 -0800

    organized arm asm flags

--- r5_asm/build.sh
+++ r5_asm/build.sh
@@ -6,4 +6,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd "${SCRIPT_DIR}"
 
-/tools/Xilinx/Vitis/2020.1/gnu/armr5/lin/gcc-arm-none-eabi/bin/armr5-none-eabi-g++ -S -mcpu=cortex-r5 -mfloat-abi=hard -mfpu=vfpv3-d16 -Wa,-adhln -fverbose-asm -fno-asynchronous-unwind-tables -fno-exceptions -fno-rtti main.cpp
+# NOTE: GCC ARM Intrinsics
+# https://gcc.gnu.org/onlinedocs/gcc/ARM-C-Language-Extensions-_0028ACLE_0029.html
+
+# NOTE: Look for vfpv3-d16 in the arm7-r reference manual
+# it indicates that this is the VFPv3 unit with 16 double word
+# -mfpu=vfpv3-d16
+# -mfpu=neon
+ARCH_FLAG="-mcpu=cortex-r5 -mfloat-abi=hard"
+NICE_ASM_FLAG="-Wa,-adhln -fverbose-asm -fno-asynchronous-unwind-tables -fno-exceptions -fno-rtti "
+VECTORIZE_FLAG="-O1 -ftree-vectorize -fopt-info-vec-all -fopt-info"
+/tools/Xilinx/Vitis/2020.1/gnu/armr5/lin/gcc-arm-none-eabi/bin/armr5-none-eabi-g++ -S \
+    ${ARCH_FLAG} \
+    ${NICE_ASM_FLAG} \
+    ${VECTORIZE_FLAG} \
+    -mfpu=neon-vfpv3 \
+    main.cpp
--- r5_asm/main.cpp
+++ r5_asm/main.cpp
@@ -1,5 +1,32 @@
+#include <cstdint>
+#include <cstddef>
+
+#include <arm_neon.h>
+
+extern "C" void add_ints(int * __restrict pa, int * __restrict pb, unsigned int n, int x)
+{
+    unsigned int i;
+
+    for(i = 0; i < (n & ~3); i++)
+        pa[i] = pb[i] + x;
+}
+
 extern "C" void myFunc()
 {
-    int number;
-    number += 1;
+    constexpr size_t matrixLength = 2;
+
+    // const int32_t matrixA[matrixLength] = {1, 2, 3 ,4, 5};
+    // const int32_t matrixB[matrixLength] = {6, 7, 8 ,9, 10};
+
+    const int32_t matrixA[matrixLength] = {1, 2};
+    const int32_t matrixB[matrixLength] = {6, 7};
+
+    // int32x2_t arr;
+
+    int32_t matrixRes[matrixLength] = {};
+
+    for(size_t i = 0; i < matrixLength; ++i)
+    {
+        matrixRes[i] = matrixA[i] + matrixB[i];
+    }
 }
